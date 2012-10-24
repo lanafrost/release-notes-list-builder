7d0a9634f57c4c8c9b58efabe6586e619354e465
Mark Craig <mark.craig@gmail.com>
2012-10-24 08:54:33 +0200

    Wiki output support

---
+++ src/main/python/menu2wiki.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# If applicable, add the following below this MPL 2.0 HEADER, replacing
+# the fields enclosed by brackets "[]" replaced with your own identifying
+# information:
+#     Portions Copyright [yyyy] [name of copyright owner]
+#
+#     Copyright 2012 ForgeRock AS
+
+import argparse, ConfigParser, json, os, sys, urllib2
+from xml.sax.saxutils import escape
+
+def clearTerminal():
+	os.system( [ 'clear', 'cls' ][ os.name == 'nt' ] )
+	return
+
+def getSelection(queries):
+	totalItems = len(queries) + 1
+	
+	clearTerminal()
+
+	while True:
+		print 'List of available JIRA queries'
+		print '=============================='
+		print
+		i = 1
+		for k,v in queries:
+			print '\t', i, ')' , k
+			i = i+1
+		print
+				
+		try:
+			answer = int(raw_input('Enter your selection: '))
+		except ValueError:
+			print 'Invalid selection:', answer
+			continue
+	
+		if not answer in range(1, totalItems):
+			print
+			print 'You selected:', answer, '(valid range: 1 -', totalItems - 1, ')'
+			print
+			continue
+	
+		return answer
+
+def dumpWikiText(url, outfile):
+	response = urllib2.urlopen(url).read()
+	jira = json.loads(response)
+
+	if outfile == sys.stdout:
+		clearTerminal()
+	
+	# This part depends on the JIRA JSON object's structure...
+	for issue in jira["issues"]:
+		id = issue["key"]
+		desc = escape(issue["fields"]["summary"])
+
+		outfile.write('* [')
+		outfile.write(id)
+		outfile.write('|https://bugster.forgerock.org/jira/browse/')
+		outfile.write(id)
+		outfile.write(']: ')
+		outfile.write(desc)
+		outfile.write('\n')
+		outfile.flush()
+	
+	return
+
+parser = argparse.ArgumentParser()
+parser.add_argument('file', help='dump wikitext output to file', nargs='?',
+							type=argparse.FileType('w'), default=sys.stdout)
+args = parser.parse_args()
+
+config = ConfigParser.RawConfigParser()
+config.optionxform=str
+config.read('queries.cfg')	
+queries = config.items('Queries')
+
+url = queries[getSelection(queries) - 1][1]		# Selection starts at 1.
+												# List index starts at 0.
+												# Tuples are (name, url).
+dumpWikiText(url, args.file)
